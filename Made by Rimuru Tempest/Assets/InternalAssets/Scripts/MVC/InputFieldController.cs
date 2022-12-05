3c89ebc3d9b42ac39776e3044509ad425173d0bd
Rimuru <Rimuru.Dev@gmail.com>
2022-12-05 15:30:57 +0400

    Create InputFieldController.cs

---
+++ Made by Rimuru Tempest/Assets/InternalAssets/Scripts/MVC/InputFieldController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace RimuruDev.FromPlayclapp
+{
+    public sealed class InputFieldController : MonoBehaviour
+    {
+        [SerializeField] private StringEvent motionSpeedInputField;
+        [SerializeField] private StringEvent spawnCooldownInputField;
+        [SerializeField] private StringEvent maxDistanceInputField;
+    }
+}
